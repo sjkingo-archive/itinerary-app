148207e85635524a9ca1765f1fba7e5ccd024166
Sam Kingston <sam@sjkwi.com.au>
2013-06-06 23:52:08 +1000

    tidy up templates and views

--- transit/views.py
+++ transit/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, get_list_or_404
-import datetime
 
 from models import *
 
-def list_itinerary(req, template='list.html'):
+def list_dates(req, template='list_dates.html'):
     activities = Activity.objects.all()
     dates = set()
     for a in activities:
@@ -11,9 +10,8 @@ def list_itinerary(req, template='list.html'):
     context = {'dates': dates}
     return render(req, template, context)
 
-def day_itinerary(req, date, template='day.html'):
-    activities = get_list_or_404(Activity, date=date)
-    #activities = zip(r[:], r[1:])
+def daily_itinerary(req, this_date, template='daily_itinerary.html'):
+    activities = get_list_or_404(Activity, date=this_date)
     context = {'activities': activities,
-               'date': date}
+               'this_date': activities[0].date} # use a datetime object instead of str
     return render(req, template, context)
