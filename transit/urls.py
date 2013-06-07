6de4b490ebcf9f485800f2875ebea46e825a1864
Sam Kingston <sam@sjkwi.com.au>
2013-06-08 07:19:29 +1000

    root activity with a prefix

--- transit/urls.py
+++ transit/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'transit.views.overview', name='overview'),
-    url(r'^(\d+)/$', 'transit.views.activity', name='activity'),
+    url(r'^activity/(\d+)/$', 'transit.views.activity', name='activity'),
     url(r'^set_route_index/$', 'transit.views.set_route_index', name='set_route_index'),
 )
